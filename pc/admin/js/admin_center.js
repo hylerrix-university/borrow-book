e45da13559bf097202165a3aae3b8430c617989c
hylerrix <hylerrix@foxmail.com>
2017-06-27 18:01:17 +0800

    完善pc端操作界面一些小细节

--- pc/admin/js/admin_center.js
+++ pc/admin/js/admin_center.js
@@ -1,3 +1,49 @@
+function finishBook () {
+    var isbn = $("input:eq(0)").val();
+    var post_data = {
+        "isbn": isbn
+    };
+    var post_url = "https://corefuture.cn/lib_api/book/finishBook.action";
+    // $.post(post_url, post_data, function (data, status) {
+        // alert(data);
+        $(".pContentDetailWrap:eq(0)").show();
+        $(".pContentSubmitWrap:eq(0)").show();
+    // });
+}
+
+function saveBook () {
+    var isbn = $("input:eq(0)").val();
+    var post_data = {
+        "isbn": isbn
+    };
+    var post_url = "https://corefuture.cn/lib_api/book/finishBook.action";
+    $.post(post_url, post_data, function (data, status) {
+        alert(data);
+    });
+}
+
+function getAllStacks () {
+    var post_url = "https://wwwxinle.cn/Book/public/index.php/index/Book/getAllStacks";
+    $.post(post_url, function (data, status) {
+        console.log(data);
+        setStacks_0();
+        setStacks_1();
+    });
+}
+
+// 设置录入书籍选项卡下的单选框二级联动
+function setStacks_0 () {
+
+}
+
+// 设置书库管理选项卡下的单选框二级联动
+function setStacks_1 () {
+
+}
+
+getAllStacks();
+
+
 function changeNaviContent (index) {
     if (index == 0) {
 	    // 录入书籍
