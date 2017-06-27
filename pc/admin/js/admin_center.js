66c328870cfc3e35e0319648eaf1a7115d3145e7
hylerrix <hylerrix@foxmail.com>
2017-06-27 15:37:47 +0800

    PC端页面模板已完成

--- pc/admin/js/admin_center.js
+++ pc/admin/js/admin_center.js
@@ -1,16 +1,32 @@
 function changeNaviContent (index) {
-	if (index == 0) {
-		$(".pNavigationItem:eq(0) button").css("background-color", "rgba(0, 0, 0, 0)");
-		$(".pNavigationItem:eq(1) button").css("background-color", "rgba(0, 0, 0, .2)");
-		$(".pContentWrap_0").show();
-		$(".pContentWrap_1").hide();
-	} else {
-		console.log(index);
-		$(".pNavigationItem:eq(1) button").css("background-color", "rgba(0, 0, 0, 0)");
-		$(".pNavigationItem:eq(0) button").css("background-color", "rgba(0, 0, 0, .2)");
-		$(".pContentWrap_1").show();
-		$(".pContentWrap_0").hide();
-	}
+    if (index == 0) {
+	    // 录入书籍
+        $(".pNavigationItem:eq(0) button").css("background-color", "rgba(0, 0, 0, 0)");
+        $(".pNavigationItem:eq(1) button").css("background-color", "rgba(0, 0, 0, .2)");
+        $(".pNavigationItem:eq(2) button").css("background-color", "rgba(0, 0, 0, .2)");
+        $(".pContentWrap_0").show();
+        $(".pContentWrap_1").hide();
+        $(".pContentWrap_2").hide();
+    } else if (index == 1) {
+        // 新建书库
+        $(".pNavigationItem:eq(0) button").css("background-color", "rgba(0, 0, 0, .2)");
+        $(".pNavigationItem:eq(1) button").css("background-color", "rgba(0, 0, 0, 0)");
+        $(".pNavigationItem:eq(2) button").css("background-color", "rgba(0, 0, 0, .2)");
+        $(".pContentWrap_0").hide();
+        $(".pContentWrap_1").show();
+        $(".pContentWrap_2").hide();
+    } else if (index == 2) {
+        // 管理员账号管理
+        $(".pNavigationItem:eq(0) button").css("background-color", "rgba(0, 0, 0, .2)");
+        $(".pNavigationItem:eq(1) button").css("background-color", "rgba(0, 0, 0, .2)");
+        $(".pNavigationItem:eq(2) button").css("background-color", "rgba(0, 0, 0, 0)");
+        $(".pContentWrap_0").hide();
+        $(".pContentWrap_1").hide();
+        $(".pContentWrap_2").show();
+    }
 }
 
-changeNaviContent(0);
+var urlArgs = window.location.search;
+var tabIndex = urlArgs.split("=")[1];
+if(!tabIndex) tabIndex = 0;
+changeNaviContent(tabIndex);
