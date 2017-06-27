041c9812c7dc7f8895c6f1a9f0826a7f2c3c1fb8
hylerrix <hylerrix@foxmail.com>
2017-06-27 16:42:15 +0800

    完成登录验证

--- app/admin/js/admin_login.js
+++ app/admin/js/admin_login.js
@@ -1,5 +1,21 @@
 function login () {
-	window.location.href = "admin_center.html";
+	var admin_name = $("input:eq(0)").val();
+	var admin_password = $("input:eq(1)").val();
+	var post_data = {
+		"admin_name": admin_name,
+		"admin_password": admin_password,
+		"admin_pid": 1
+	}
+	var post_url = "https://wwwxinle.cn/Book/public/index.php/index/Manager/login";
+	$.post(post_url, post_data, function (data, status) {
+		if (data["res"] == 1) {
+			window.location.href = "admin_center.html";
+		} else {
+			$(".mTitleHeader:eq(1)").text("账号或密码错误");
+			$(".mTitleHeader:eq(1)").show();
+		}
+	});
+	return;
 }
 
 function logout () {
