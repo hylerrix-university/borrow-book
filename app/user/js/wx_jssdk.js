5d864d07e8a208c7792d3bf370787e407df85570
hylerrix <hylerrix@foxmail.com>
2017-06-27 14:47:03 +0800

    加入jssdk分享接口

--- app/user/js/wx_jssdk.js
+++ app/user/js/wx_jssdk.js
@@ -66,18 +66,55 @@ function jsSDK (afterScan) {
         console.log("jsapi_ticket.signature: " + jsapi_ticket.signature);
 
         wx.config({
-            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端 alert 出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端 alert 出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: 'wx1a474de918181499', // 必填，公众号的唯一标识
             timestamp: jsapi_ticket.timestamp, // 必填，生成签名的时间戳
             nonceStr: jsapi_ticket.nonceStr, // 必填，生成签名的随机串
             signature: jsapi_ticket.signature,// 必填，签名，见附录 1
-            jsApiList: ["checkJsApi", "scanQRCode"] // 必填，需要使用的 JS 接口列表，所有 JS 接口列表见附录2
+            jsApiList: [ // 必填，需要使用的 JS 接口列表，所有 JS 接口列表见附录2
+                "checkJsApi",
+                "scanQRCode",
+                "onMenuShareTimeline",
+                "onMenuShareAppMessage",
+            ]
         });
 
         // 通过ready接口处理成功验证
         wx.ready(function() {
             // config信息验证后会执行 ready 方法，所有接口调用都必须在 config 接口获得结果之后，config 是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在 ready 函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在 ready 函数中。
             console.log("wx.ready");
+            wx.onMenuShareTimeline({
+                // 分享到朋友圈
+                title: '测试号此功能暂无法使用', // 分享标题
+                link: 'https://baidu.com', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                imgUrl: 'https://www.baidu.com/img/bd_logo1.png', // 分享图标
+                trigger: function (res) {
+                    alert('用户点击分享到朋友圈');
+                },
+                success: function () { 
+                    // 用户确认分享后执行的回调函数
+                    console.log("success");
+                },
+                cancel: function () { 
+                    // 用户取消分享后执行的回调函数
+                    console.log("cancel");
+                }
+            });
+            wx.onMenuShareAppMessage({
+                // 分享给朋友
+                title: '测试号此功能暂无法使用', // 分享标题
+                desc: '测试号此功能暂无法使用', // 分享描述
+                link: 'https://baidu.com', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                imgUrl: 'https://www.baidu.com/img/bd_logo1.png', // 分享图标
+                type: 'link', // 分享类型,music、video或link，不填默认为link
+                dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                success: function () { 
+                    // 用户确认分享后执行的回调函数
+                },
+                cancel: function () { 
+                    // 用户取消分享后执行的回调函数
+                }
+            });
         });
 
         // 通过error接口处理失败验证
